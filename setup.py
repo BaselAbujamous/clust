72e4b7ca411fd5c136d26b3c1093a2c9d6623473
Basel Abu Jamous <basel.abujamous@plants.ox.ac.uk>
2017-03-16 15:53:12 +0000

    Moved the entry point to a new module (__main__) and fixes setup accordingly.

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ setup(
     # pip to create the appropriate form of executable for the target platform.
     entry_points={
         'console_scripts': [
-            'clust=clust.clust',
+            'clust=clust.__main__:main',
         ],
     },
 )
